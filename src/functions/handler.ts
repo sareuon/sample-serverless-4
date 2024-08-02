c80ac876692efc42cf36144183936dc361340e50
Soum Sareuon <soum.sareuon@gmail.com>
2024-08-02 14:57:55 +0700

    add lambda function

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -13,59 +13,60 @@ interface TiffData {
 const s3 = new S3Client({ region: 'us-east-1' });
 const bucket = 'cog-tesing';
 
-export const tile = async ( event: APIGatewayProxyEvent,_context: Context ): Promise<APIGatewayProxyResult> => {
-    try {
+// Constants
+const tileSize = 256;
+const webMercatorExtent = 20037508.3427892;
 
-      // Extract z, x, y from the path parameters
-      const { z, x, y } = event.pathParameters as {
-        z: string;
-        x: string;
-        y: string;
-      };
-
-    // Calculate the bounding box for the tile
-    const tileSize = 256;
-    const resolution = 0.00028 * Math.pow(2, parseInt(z));
-    const bbox: [number, number, number, number] = [
-      parseInt(x) * tileSize * resolution,
-      parseInt(y) * tileSize * resolution,
-      (parseInt(x) + 1) * tileSize * resolution,
-      (parseInt(y) + 1) * tileSize * resolution,
-    ];
-
-    const { buffer, contentType } = await fetchTiffData(bbox, tileSize);
-
-
-      return {
-        // statusCode: 200,
-        // body: JSON.stringify({
-        //   message: 'Hello, world!',
-        //   input: event,
-        // }),
-        statusCode: 200,
-        headers: {
-          'Content-Type': contentType,
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Access-Control-Allow-Origin',
-          'Access-Control-Allow-Methods': 'OPTIONS,GET'
-        },
-        body: buffer.toString('base64'),
-        isBase64Encoded: true,
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Access-Control-Allow-Origin',
-          'Access-Control-Allow-Methods': 'OPTIONS,GET'
-        },
-        body: JSON.stringify({ error: `An error occurred ${error}` }),
+export const tile: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+  _context: Context
+): Promise<APIGatewayProxyResult> => {
+  try {
+    // Extract z, x, y from the path parameters
+    const { z, x, y } = event.pathParameters as {
+      z: string;
+      x: string;
+      y: string;
     };
-  };
+
+    const webMercatorBbox: [number, number, number, number] = tileToWebMercator(
+      parseInt(x),
+      parseInt(y),
+      parseInt(z)
+    );
+    const { buffer, contentType } = await fetchTiffData(webMercatorBbox, tileSize);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': contentType,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Access-Control-Allow-Origin',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+      },
+      body: buffer.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,Access-Control-Allow-Origin',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+      },
+      body: JSON.stringify({ error: `An error occurred ${error}` }),
+    };
+  }
 };
 
-const fetchTiffData = async (bbox: [number, number, number, number], tileSize: number): Promise<TiffData> => {
+const fetchTiffData = async (
+  bbox: [number, number, number, number],
+  tileSize: number
+): Promise<TiffData> => {
+
   const { fromUrl } = await import('geotiff');
   const key = 'output_cog.tif';
   const url = await getSignedUrl(
@@ -76,11 +77,24 @@ const fetchTiffData = async (bbox: [number, number, number, number], tileSize: n
 
   const tiff = await fromUrl(url);
   const image = await tiff.getImage();
-  const rasters = await image.readRasters({ window: bbox });
 
+  // Log image metadata
+  const imageWidth = image.getWidth();
+  const imageHeight = image.getHeight();
+  const geoTransform = image.getGeoKeys();
+  console.log('Image Metadata:', { imageWidth, imageHeight, geoTransform });
+
+  console.log(bbox);
+  const [imageMinX, imageMinY, imageMaxX, imageMaxY] = image.getBoundingBox();
+  console.log('Image Bounding Box:', [imageMinX, imageMinY, imageMaxX, imageMaxY]);
+  
+
+
+  // const rasterBbox = await webMercatorToPixel(bbox, image);
+  const rasters = await image.readRasters({ window: bbox});
+  
   // Convert rasters to PNG
   const buffer = await convertRastersToPng(rasters, tileSize, tileSize);
-
   return {
     buffer,
     contentType: "image/png"
@@ -89,10 +103,10 @@ const fetchTiffData = async (bbox: [number, number, number, number], tileSize: n
 
 // Function to convert rasters to PNG using sharp
 const convertRastersToPng = async (
-    rasters: any,
-    width: number,
-    height: number
-  ) => {
+  rasters: any,
+  width: number,
+  height: number
+): Promise<Buffer> => {
 
   // Combine the raster bands into an interleaved RGBA buffer
   const rgbaBuffer = Buffer.alloc(width * height * 4);
@@ -101,7 +115,7 @@ const convertRastersToPng = async (
     rgbaBuffer[i * 4] = rasters[0][i]; // Red
     rgbaBuffer[i * 4 + 1] = rasters[1][i]; // Green
     rgbaBuffer[i * 4 + 2] = rasters[2][i]; // Blue
-    rgbaBuffer[i * 4 + 3] = 255; // Alpha
+    rgbaBuffer[i * 4 + 3] = 0; // Alpha
   }
 
   // Use sharp to convert the RGBA buffer to a PNG buffer
@@ -112,4 +126,49 @@ const convertRastersToPng = async (
     .toBuffer();
 
   return pngBuffer;
+};
+
+// Corrected function to convert Web Mercator coordinates to pixel coordinates
+const webMercatorToPixel = (
+  bbox: [number, number, number, number],
+  image: any
+): [number, number, number, number] => {
+  const [minX, minY, maxX, maxY] = bbox;
+  const imageWidth = image.getWidth();
+  const imageHeight = image.getHeight();
+  const [imageMinX, imageMaxY] = image.getOrigin();
+  const imageResolution = image.getResolution
+    ? image.getResolution()
+    : webMercatorExtent * 2 / imageWidth;
+
+  const pixelMinX = Math.round((minX - imageMinX) / imageResolution);
+  const pixelMaxY = Math.round((imageMaxY - minY) / imageResolution);
+  const pixelMaxX = Math.round((maxX - imageMinX) / imageResolution);
+  const pixelMinY = Math.round((imageMaxY - maxY) / imageResolution);
+
+  // Ensure pixel values are within the image dimensions
+  const boundedPixelMinX = Math.max(0, Math.min(pixelMinX, imageWidth));
+  const boundedPixelMaxX = Math.max(0, Math.min(pixelMaxX, imageWidth));
+  const boundedPixelMinY = Math.max(0, Math.min(pixelMinY, imageHeight));
+  const boundedPixelMaxY = Math.max(0, Math.min(pixelMaxY, imageHeight));
+
+  return [boundedPixelMinX, boundedPixelMinY, boundedPixelMaxX, boundedPixelMaxY];
+};
+
+// Function to convert tile coordinates to Web Mercator coordinates
+const tileToWebMercator = (
+  x: number,
+  y: number,
+  z: number
+): [number, number, number, number] => {
+  const resolution = getResolution(z);
+  const minX: number = x * tileSize * resolution - webMercatorExtent;
+  const maxY: number = webMercatorExtent - y * tileSize * resolution;
+  const maxX: number = (x + 1) * tileSize * resolution - webMercatorExtent;
+  const minY: number = webMercatorExtent - (y + 1) * tileSize * resolution;
+  return [minX, minY, maxX, maxY];
+};
+
+const getResolution = (z: number): number => {
+  return (2 * webMercatorExtent) / (tileSize * Math.pow(2, z));
 };
